863309a2e22671906ca836b6a6c88894b844ae63
wang115t//自定义用户名 <2690761457@qq.com//>
2024-07-09 18:35:30 +0800

    更新版本号1.2.3

--- background/version.py
+++ background/version.py
@@ -2,11 +2,13 @@ __version__ = "1.2.3"
 release_date = "2024-07-09"
 description = "更新"
 
-
 # ver1.2.3
 # update:2024-07-08
-# updated by wakening
-# 1.支持国服、b服账号登录弹窗识别，并自动点击登录
+# updated by wangll5t
+# 1.新增智能场景动态切换GPU/CPU识别的功能，有效的缓解GPU爆显存的问题
+# 2.在config.yaml配置，实现性功能
+
+
 
 # ver1.2.2
 # update:2024-07-08
