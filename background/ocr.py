b408bf10a8f051886480b7f549511c86e2af4fdb
wang115t//自定义用户名 <2690761457@qq.com//>
2024-07-09 18:34:24 +0800

    新增配置动态切换CPU/GPU的功能

--- background/ocr.py
+++ background/ocr.py
@@ -13,14 +13,12 @@ import numpy as np
 from schema import OcrResult, Position
 from config import config
 import logging
-from status import logger
+from status import logger,info
 
 
 ocrIns: PaddleOCR = None
 
-if paddle.is_compiled_with_cuda() and paddle.get_device().startswith(
-    "gpu"
-):  # 判断是否调用GPU
+if paddle.is_compiled_with_cuda() and paddle.get_device().startswith("gpu"):  # 判断是否调用GPU
     use_gpu = True
 else:
     use_gpu = False
@@ -31,6 +29,41 @@ if current_process().name == "task":
     ocrIns = PaddleOCR(use_angle_cls=False, use_gpu=use_gpu, lang="ch", show_log=False)
 
 last_time = time.time()
+
+def ocr_switch():
+    info.useGpu = not info.useGpu
+
+def ocr_switch_cpu():
+    if info.useGpu == True:
+        info.useGpu = False
+
+def ocr_switch_gpu():
+    if info.useGpu == False:
+        info.useGpu = True
+
+status_use:bool = None
+def init_ocr():
+    if config.IntelligentSceneSwitching:
+        global ocrIns
+        global use_gpu
+        global status_use
+        if use_gpu == True:
+            if status_use !=None and (status_use == info.useGpu):
+            #  logger(f"默认使用【GPU】识别","DEBUG") if info.useGpu == True else logger(f"默认使用【CPU】识别","DEBUG")
+                if info.useGpu:
+                    logger(f"默认使用【GPU】识别","DEBUG")
+                else:
+                    logger(f"默认使用【CPU】识别","DEBUG")
+                return
+            elif (status_use != info.useGpu):
+                use_gpu_t = info.useGpu
+                status_use  = use_gpu_t
+                ocrIns = PaddleOCR(use_angle_cls=False, use_gpu=use_gpu_t, lang="ch",show_log=False)
+                # logger(f"使用GPU识别") if use_gpu_t == True else logger(f"使用CPU识别")
+                if use_gpu_t:
+                    logger(f"使用【GPU】识别","DEBUG")
+                else:
+                    logger(f"使用【CPU】识别","DEBUG")
 
 
 def ocr(img: np.ndarray) -> list[OcrResult]:
