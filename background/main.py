c25505a0554223660395f72941f6a8227df549cd
wakening <32231663+wakening@users.noreply.github.com>
2024-08-18 16:08:29 +0800

    修复F7/F12异常，删除F9日志

--- background/main.py
+++ background/main.py
@@ -189,6 +189,7 @@ def on_press(key):
         logger("启动BOSS脚本")
         thread = Process(target=run, args=(boss_task, taskEvent), name="task")
         thread.start()
+        mouse_reset_thread = Process(target=mouse_reset, args=(mouseResetEvent,), name="mouse_reset")
         mouse_reset_thread.start()
         cache_process_dict("boss_task", thread)
         cache_process_dict("mouse_reset_thread", mouse_reset_thread)
@@ -208,6 +209,7 @@ def on_press(key):
     if key == Key.f7:
         logger("暂停脚本")
         taskEvent.clear()
+        mouseResetEvent.clear()
         force_close_process()
         logger("暂停执行完成")
     if key == Key.f8:
@@ -218,6 +220,7 @@ def on_press(key):
     if key == Key.f12:
         logger("请等待程序退出后再关闭窗口...")
         taskEvent.clear()
+        mouseResetEvent.clear()
         # logger(str(process_dict))
         force_close_process()
         restart_thread.terminate()
@@ -278,9 +281,6 @@ def cmd_task_func(cmd_task_dict: OrderedDict[str, Key]):
 if __name__ == "__main__":
     taskEvent = Event()  # 用于停止任务线程
     mouseResetEvent = Event()  # 用于停止鼠标重置线程
-    mouse_reset_thread = Process(
-        target=mouse_reset, args=(mouseResetEvent,), name="mouse_reset"
-    )
     restart_thread = Process(
         target=restart_app, args=(taskEvent,), name="restart_event"
     )
@@ -300,7 +300,7 @@ if __name__ == "__main__":
     )
     print("请确认已经配置好了config.yaml文件\n")
     print(
-        "使用说明：\n   F5  启动脚本\n   F6  合成声骸\n   F7  暂停运行\n   F8  锁定声骸\n   F9  声骇得分计算\n   F12 停止运行"
+        "使用说明：\n   F5  启动脚本\n   F6  合成声骸\n   F7  暂停运行\n   F8  锁定声骸\n   F12 停止运行"
     )
     logger("开始运行")
     run_cmd_tasks_async()
